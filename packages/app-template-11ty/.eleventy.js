f7ba8662efd030e2177d67d1d98b0de41ce40413
Tony Sullivan <tony.f.sullivan@outlook.com>
2020-07-16 12:07:35 +0200

    app-template-11ty refactoring project structure

--- packages/app-template-11ty/.eleventy.js
+++ packages/app-template-11ty/.eleventy.js
@@ -1,23 +1,19 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    // Templates:
-    'html',
-    'njk',
-    'md',
-    // Static Assets:
-    'css',
-    'svg',
-    'png',
-    'jpg',
-    'jpeg',
-  ])
-  eleventyConfig.addPassthroughCopy('static')
+  eleventyConfig.addPassthroughCopy({ static: '/' })
 
   return {
+    templateFormats: ['md', 'njk', 'html', '11ty.js'],
+
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+
     dir: {
       input: '_template',
       includes: '../_includes',
+      input: 'site',
+      includes: '_includes',
       output: '_output',
-    },
+      data: '_data'
+    }
   }
 }
